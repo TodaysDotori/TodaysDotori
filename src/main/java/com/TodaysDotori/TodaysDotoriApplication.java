ae45edac9a137eae6d8fb36da938df2a7ebf1378
104735K <shk773577@gmail.com>
2024-12-09 15:55:56 +0900

    setting / gitignore

---
+++ src/main/java/com/TodaysDotori/TodaysDotoriApplication.java
@@ -0,0 +1,13 @@
+package com.TodaysDotori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodaysDotoriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodaysDotoriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TodaysDotori/TodaysDotoriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TodaysDotori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodaysDotoriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
